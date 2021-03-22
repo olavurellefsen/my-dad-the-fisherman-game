739efd94ca106fb36069fc80a2b73fa81285c4b2
Kristmund Ryggstein <kristmunds@hotmail.com>
2021-03-22 11:14:29 +0000

    changed tøkni link color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="container text-right text-secondary">
-    <a href="https://www.tokni.com">
-      Developed by Tøkni
+    <a href="https://www.tokni.com" style={{color: "black"}}>
+      Tøkni hevur ment
     </a>
   </footer>
 );
